ad2530b861cf1e91cc92920f81344d9b2510c320
brckhan88 <brckhan88@gmail.com>
2020-07-16 02:50:01 +0300

    Android

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -339,6 +339,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-statusbar": "2.4.2",
     "cordova-plugin-whitelist": "1.3.3",
     "cordova-plugin-googlemaps": "2.7.1",
-    "cordova-plugin-ios-camera-permissions": "1.2.0"
+    "cordova-plugin-ios-camera-permissions": "1.2.0",
+    "cordova-support-google-services": "1.4.1"
   };
 });
