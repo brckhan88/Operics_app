da6155405107c8f5b55c72f4901fb7e69ed5bb36
brckhan88 <brckhan88@gmail.com>
2020-07-20 16:28:09 +0300

    Update webservice.php

--- www/webservice.php
+++ www/webservice.php
@@ -512,12 +512,13 @@ switch ($service_type) {
     break;
 
     case "catchPP":
+
         $deal_pictureUrl = $data["photoLink"];
-        $urldate         = new DateTime(date("Y-m-d H:i:s"));
+        $urldate         = new DateTime(date('Y-m-d H:i:s'));
 
-        file_put_contents("img/deals/". $urldate . ".jpeg", base64_decode(preg_replace("#^data:image/\w+;base64,#i", "", $deal_pictureUrl)));
+        file_put_contents("img/pics/". $urldate . ".jpeg", base64_decode(preg_replace("#^data:image/\w+;base64,#i", "", $deal_pictureUrl)));
 
-        $photo_url = "http://www.microwebservice.net/operics_web/img/%22.$urldate.%22.jpeg";
+        $photo_url = "http://www.microwebservice.net/operics_web/img/pics/%22.$urldate.%22.jpeg";
         $rows[]=["photo_link"=>$photo_url];
 
         print json_encode($rows, JSON_UNESCAPED_UNICODE);
